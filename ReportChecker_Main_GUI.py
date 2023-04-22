e34c4378c8e188f3f6b7d45b23b1c057b69ba40c
Boonkai <boonkai.yeam@gmail.com>
2023-04-22 12:35:58 +0800

    Create an input file on main screen manager
    1. Main screen manager input can be now automatically insert into development tool.
    
    2 Create a Report files input file frame container to hold NetPin GUI design
    
    3. Reposition close,open development tool and Exit button

--- ReportChecker_Main_GUI.py
+++ ReportChecker_Main_GUI.py
@@ -1,5 +1,6 @@
 from Background_GUI_Tool import *
 from tkinter import *
+from HtmlDataExtraction import *
 
 
 class mainGUI:
@@ -7,25 +8,51 @@ class mainGUI:
         Background_GUI()
         self.main_Gui_root = Tk()
         self.main_Gui_root.title("Main screen")
+        self.ReportFile_Frame = LabelFrame(self.main_Gui_root,text="Report Files Input",labelanchor="n")
+        self.ReportFile_Frame.grid(row=0,column=1,columnspan=5)
         self.main_Gui_root.protocol("WM_DELETE_WINDOW",self.pressExit)
 
-        self.main_entry = Entry(self.main_Gui_root,width=5,background='white',fg="black",borderwidth=3)
-        self.main_entry.grid(row=0,column=0)
+        #--------------------NetPin tkinder GUI------------------------------------#
+        # create a label for the input field
+        self.NetPin_main_input_label = Label(self.ReportFile_Frame, text="Component Pin File:")
+        self.NetPin_main_input_label.grid(row=0, column=0)
+    
+        self.NetPin_main_input_entry = Entry(self.ReportFile_Frame ,width=70,background='white',fg="black",borderwidth=3)
+        self.NetPin_main_input_entry.grid(row=0,column=1)
 
-        presshow = Button(self.main_Gui_root, text="hide background",command=self.hidebackground)
-        presshow.grid(row=1,column=0)
+        self.NetPin_main_browser_button = Button(self.ReportFile_Frame , text="Browse",command=self.NetPin_main_browse_file)
+        self.NetPin_main_browser_button.grid(row=0, column=2)
 
-        presshow = Button(self.main_Gui_root, text="show background",command=self.showBack)
-        presshow.grid(row=2,column=0)
+        #-------------------Hide, Show,Exit Button-------------------------------#
+        self.presshow = Button(self.main_Gui_root, text="Close Development Tool",command=self.hidebackground)
+        self.presshow.grid(row=1,column=1)
 
-        pressexit = Button(self.main_Gui_root, text="Exit",command=self.pressExit)
-        pressexit.grid(row=3,column=0)
+        self.presshow = Button(self.main_Gui_root, text="Open Development Tool",command=self.showBack)
+        self.presshow.grid(row=1,column=2)
+
+        self.pressexit = Button(self.main_Gui_root, text="Exit",command=self.pressExit)
+        self.pressexit.grid(row=1,column=4)
 
         #-------------------Create the export button--------------------------#
-        export_button = Button(self.main_Gui_root, text="Generate Report", command=export_report)
-        export_button.grid(row=4,column=0)
+        self.export_button = Button(self.main_Gui_root, text="Generate Report", command=export_report)
+        self.export_button.grid(row=1,column=3)
 
-        
+
+    # create a function to open the file browser and select a file
+    def NetPin_main_browse_file(self):
+        # Component Pin Report File input:
+        self.NetPin_main_file_path = filedialog.askopenfilename(initialdir="/Users/yeamboonkai/Desktop/AMD_Project/Input", 
+                                            title="Select a File", 
+                                            filetypes=(("htm", "*.htm"), ("All files", "*.*")))
+        self.NetPin_main_input_entry.delete(0, END)
+        self.NetPin_main_input_entry.insert(0, self.NetPin_main_file_path)
+
+        # NetPin_Gui_obj.NetPin_file_path = self.NetPin_main_file_path
+        NetPin_Gui_obj.NetPin_input_entry.delete(0,END)
+        NetPin_Gui_obj.NetPin_input_entry.insert(0,self.NetPin_main_file_path)
+
+        # Update NetPin directory path label
+        NetPin_Gui_obj.NetPin_file_path_label.config(text=self.NetPin_main_file_path)
 
     def pressExit(self):
         BackGui_root.destroy()
